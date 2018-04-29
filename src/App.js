c5fa9df748a35885c78f9f2e3c0bff695a70f4bf
itsGregT <webdeveloper@gregthomas.me>
2018-04-28 20:31:12 -0500

    refactor task

--- src/App.js
+++ src/App.js
@@ -10,11 +10,17 @@ class App extends Component {
   }
 
   addTask = task => {
-    //make copy of current state
     const tasks = {...this.state.tasks};
     //add new task
     tasks[`task${Date.now()}`] = task;
     this.setState({ tasks })
+  }
+
+  deleteTask = index => {
+    const tasks = {...this.state.tasks};   
+
+    console.log(Object.keys(tasks));
+    console.log(index);
   }
 
   render() {
@@ -24,7 +30,7 @@ class App extends Component {
         <AddTaskForm addTask={this.addTask} />
         <div className="task-list">
           {Object.keys(this.state.tasks).map(key =>  
-            <Task key={key} details={this.state.tasks[key]} index={key}/>
+            <Task key={key} details={this.state.tasks[key]} deleteTask={this.deleteTask} index={key}/>
           )}
         </div>
       </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -2,19 +2,23 @@ import React from 'react';
 import '../css/Task.css';
 
 class Task extends React.Component {
+	removeTask = () => {
+		this.props.deleteTask(this.props.index);
+	}
+
 	render() {
 		const {title, desc} = this.props.details;
 		return (
 			<div className="task">
 				<div className="task-header">
 					<h3>{title}</h3>
-					<span className="delete-task">X</span>
+					<span className="delete-task" onClick={this.removeTask}>X</span>
 				</div>
 				<div className="task-content">
 					<p>{desc}</p>
 				</div>
 				<div className="task-toggle">
-					<input id="active" type="radio" name={this.props.index} checked readonly/>
+					<input id="active" type="radio" name={this.props.index} checked readOnly/>
 					<label htmlFor="active" className="toggle toggle-left">To-Do</label>
 					<input id="inactive" type="radio" name={this.props.index} />
 					<label htmlFor="inactive" className="toggle toggle-right">Complete</label>
